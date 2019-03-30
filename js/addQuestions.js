73bc68dbbdf022b413b3a18e3aaec9bd85a96847
atorres98 <37823055+atorres98@users.noreply.github.com>
2019-03-30 13:26:32 -0400

    createLongAnswer function

--- js/addQuestions.js
+++ js/addQuestions.js
@@ -7,6 +7,16 @@ function createShortAnswer() {
 }
 
 function createLongAnswer() {
+		var question = document.getElementById("longAnswer");
+		var input = document.createElement("input");
+		input.type = "text";
+		question.appendChild(input);
+		// question.appendChild(break);
+	
+		// Delete question button
+		var btn = document.createElement("BUTTON");
+		btn.innerHTML = "DELETE";
+		document.body.appendChild(btn);
 
 }
 

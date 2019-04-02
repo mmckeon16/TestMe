aa80e94223bed5b78c4faf3f1e3e065572621c42
atorres98 <37823055+atorres98@users.noreply.github.com>
2019-04-01 22:45:38 -0400

    adding the delete button

--- js/addQuestions.js
+++ js/addQuestions.js
@@ -7,26 +7,35 @@ function createShortAnswer() {
     input.placeholder = "Insert Text for Short Answer";
     input.required = true;
     container.appendChild(input);
-    container.appendChild(breakElem);
+
+	// Delete question button
+	var btn = document.createElement("BUTTON");
+	btn.innerHTML = "DELETE";
+	btn.type = "button";
+	// btn.onclick = deleteQuestion();
+	container.appendChild(btn);
+	container.appendChild(breakElem);
 
     return false;
 }
 
 function createLongAnswer() {
-	var question = document.getElementById("addedQuestions");
+	var container = document.getElementById("addedQuestions");
 	var input = document.createElement("input");
-	var breakEl = document.createElement("br");
+	var breakElem = document.createElement("br");
 	input.type = "text";
 	input.name = "longAnswer";
 	input.placeholder = "Insert Text for Long Answer";
 	input.required = true;
-	question.appendChild(input);
-	question.appendChild(breakEl);
+	container.appendChild(input);
 	
 	// Delete question button
 	var btn = document.createElement("BUTTON");
 	btn.innerHTML = "DELETE";
-	document.body.appendChild(btn);
+	container.appendChild(btn);
+	btn.type = "button";
+	// btn.onclick = deleteQuestion();
+	container.appendChild(breakElem);
 		// return false;
 
 }
@@ -48,7 +57,16 @@ function createMC() {
     options.name = "mcOptions"
     options.placeholder = "enter options as semi-colon-separated list etc: op1; op2; op3";
     options.required = true;
-    container.appendChild(options);
+	container.appendChild(options);
+	
+	// Delete question button
+	var btn = document.createElement("BUTTON");
+	btn.innerHTML = "DELETE";
+	btn.type = "button";
+	// btn.onclick = deleteQuestion();
+	container.appendChild(btn);
+	// btn.onclick(deleteQuestion);
+
     container.appendChild(breakElem);
     
     return false;
@@ -62,6 +80,18 @@ function createTrueFalse() {
     input.name = "trueFalse";
     input.placeholder = "Insert True False Question";
     input.required = true;
-    container.appendChild(input);
+	container.appendChild(input);
+
+	// Delete question button
+	var btn = document.createElement("BUTTON");
+	btn.innerHTML = "DELETE";
+	btn.type = "button";
+	// btn.onclick = deleteQuestion();
+	container.appendChild(btn);
+	// btn.onclick(deleteQuestion);
     container.appendChild(breakElem);
+}
+
+function createRanking(){
+
 }
